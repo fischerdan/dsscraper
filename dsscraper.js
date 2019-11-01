507b6565e9d84ea01df1f80eb67d760509ae09e2
Dan Fischer <dan@fischerdan.com>
2019-11-01 13:30:11 -0500

    added unique vistors as new hits

--- dsscraper.js
+++ dsscraper.js
@@ -10,8 +10,8 @@ function Article(url,title) {
   this.dateTime = "";
   this.author = "";
   this.topTag = "";
+  this.newUsers = "";
 }
-
 
 async function processPage(pageUrl){
   console.log(pageUrl);
@@ -36,48 +36,52 @@ async function processPage(pageUrl){
     };
     const articleData = await fetchArticle();
     var hits = articleData('[title*="New Visitors"]').text();
+    var newUsers = articleData('[title*="New Visitors"]').attr('title');
     const date = articleData('[class*="js_meta-time"]').parent().attr('datetime');
     const author = articleData('[data-ga*="Permalink meta\",\"Author click\""]').text();
     const tag = articleData('[data-ga*="Permalink meta\",\"Tag click\",\"First tag click\""]').text();
-    if(hits.includes("K")){
-     //most are in the thousands so drop the k
-      hits = hits.replace("K","");
+    
+    articles[url].hits = convertHitToNumber(hits);
+    if(newUsers != null){
+      articles[url].newUsers = convertHitToNumber(newUsers.replace("New Visitors",""));
     }
-    else{
-      if(hits.includes("M")){
-      // some are millions so drop the m and increas
-        hits = hits.replace("M","");
-        hits = parseFloat(hits) * 1000
-      }
-      else
-      {
-        // some are none of the above and probably under 1k
-        hits = parseFloat(hits)
-        if(isNaN(hits)){
-          hits = "0";
-        }
-        else{
-          hits = hits/1000
-        }
-      }
-    }
-    articles[url].hits = hits
     articles[url].dateTime = date;
     articles[url].author = author;
     articles[url].topTag = tag.replace("Filed to:","");
     lastDateTime = date;
   };
   const realDate = moment.utc(lastDateTime);
+  //console.log(articles);
+  //console.log(realDate.valueOf());
   writeArticlesToDB(articles);
   processPage("https://deadspin.com/?startTime=" + realDate.valueOf());
 }
 
-async function writeArticlesToDB(articles){
-  //save em some where
-
-  } catch (err) {
-    console.error(err);
+function convertHitToNumber(hits){
+  if(hits.includes("K")){
+    hits = hits.replace("K","");
   }
+  else{
+    if(hits.includes("M")){
+      hits = hits.replace("M","");
+      hits = parseFloat(hits) * 1000
+    }
+    else
+    {
+      hits = parseFloat(hits)
+      if(isNaN(hits)){
+        hits = "0";
+      }
+      else{
+        hits = hits/1000
+      }
+    }
+  }
+  return hits;
+}
+
+async function writeArticlesToDB(articles){
+  //save em somewhere
 }
 
 function isDeadspin(url){
